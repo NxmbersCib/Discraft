9ee99e799e4a09f815702f6594e9112383caf614
ImCib_ <85264290+ImCib@users.noreply.github.com>
2023-03-27 18:36:40 -0500

    Si

--- scripts/index.js
+++ scripts/index.js
@@ -19,8 +19,52 @@ world.events.playerJoin.subscribe((arg) => {
     request.setBody(
         JSON.parse(
             {
-                "content": "",
-                "username": arg.sender.name
+                "embeds": [
+                    {
+                        "title": `**${arg.playerName}**`,
+                        "color": 65280,
+                        "description": "Has joined the server!"
+                    }
+                ]
+            }
+        )
+    );
+    request.setMethod(HttpRequestMethod.POST);
+    http.request(request);
+});
+world.events.playerLeave.subscribe((arg) => {
+    const request = new HttpRequest(webHookURL);
+    request.setBody(
+        JSON.parse(
+            {
+                "embeds": [
+                    {
+                        "title": `**${arg.playerName}**`,
+                        "color": 16711680,
+                        "description": "Has left the server!"
+                    }
+                ]
+            }
+        )
+    );
+    request.setMethod(HttpRequestMethod.POST);
+    http.request(request);
+});
+world.events.entityDie.subscribe((arg) => {
+    if (arg.deadEntity.typeId != 'minecraft:player') {
+        return;
+    };
+    const request = new HttpRequest(webHookURL);
+    request.setBody(
+        JSON.parse(
+            {
+                "embeds": [
+                    {
+                        "title": `**${arg.deadEntity.name}**`,
+                        "color": 7340032,
+                        "description": arg.damageSource.cause//parseDeathCause(arg.damageSource.cause)
+                    }
+                ]
             }
         )
     );
