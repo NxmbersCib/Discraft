bdaf17a87e859dd78ca915cc70141a30b8b85a70
CibNumeritos <imcib2498.212244.2808@gmail.com>
2023-03-30 19:00:28 -0500

    casi listo tio

--- scripts/index.js
+++ scripts/index.js
@@ -2,70 +2,44 @@ import { world } from '@minecraft/server';
 import { HttpRequest, http, HttpRequestMethod, HttpHeader } from '@minecraft/server-net';
 import { deathMessages } from './IDeathMessages';
 import { webHookURL } from './setup';
-world.events.beforeChat.subscribe((arg) => {
+function weebhookMessage(messageContent = undefined, embeds = undefined) {
     const request = new HttpRequest(webHookURL);
-    request.setBody(
-        JSON.stringify(
-            {
-                "content": `[Minecraft] ${arg.sender.name}: ${arg.message}`,
-            }
-        )
-    );
+    const body = {};
+    if (messageContent) {
+        body['content'] = messageContent;
+    };
+    if (embeds) {
+        body['embeds'] = embeds;
+    };
     request.setMethod(HttpRequestMethod.POST);
+    request.setBody(JSON.stringify(body));
     request.setHeaders([new HttpHeader("Content-Type", "application/json")]);
     http.request(request);
+};
+world.events.beforeChat.subscribe((arg) => {
+    weebhookMessage(`[Minecraft] ${arg.sender.name}: ${arg.message}`);
 });
 world.events.playerJoin.subscribe((arg) => {
-    const request = new HttpRequest(webHookURL);
-    request.setBody(
-        JSON.stringify(
-            {
-                "embeds": [
-                    {
-                        "title": `**${arg.playerName}**`,
-                        "color": 65280,
-                        "description": "Has joined the server!"
-                    }
-                ]
-            }
-        )
-    );
-    request.setMethod(HttpRequestMethod.POST);
-    request.setHeaders([new HttpHeader("Content-Type", "application/json")]);
-    http.request(request);
+    weebhookMessage(undefined, [
+        { "title": `**${arg.playerName}**`, "color": 65280, "description": "Has joined the server!" }
+    ])
 });
 world.events.playerLeave.subscribe((arg) => {
-    const request = new HttpRequest(webHookURL);
-    request.setBody(
-        JSON.stringify(
-            {
-                "embeds": [
-                    {
-                        "title": `**${arg.playerName}**`,
-                        "color": 16711680,
-                        "description": "Has left the server!"
-                    }
-                ]
-            }
-        )
-    );
-    request.setMethod(HttpRequestMethod.POST);
-    request.setHeaders([new HttpHeader("Content-Type", "application/json")]);
-    http.request(request);
+    weebhookMessage(undefined, [
+        { "title": `**${arg.playerName}**`, "color": 16711680, "description": "Has left the server!" }
+    ]);
 });
 world.events.entityDie.subscribe((arg) => {
     if (arg.deadEntity.typeId != 'minecraft:player') {
         return;
     };
-    const embed = {
-        "embeds": [
-            {
-                "title": `**${arg.deadEntity.name}**`,
-                "color": 7340032,
-                "description": ""
-            }
-        ]
-    }
+    const embed = [
+        {
+            "title": `**${arg.deadEntity.name} died**`,
+            "color": 7340032,
+            "description": ""
+        }
+    ];
     /**
     * @type {string}
     */
@@ -79,33 +53,26 @@ world.events.entityDie.subscribe((arg) => {
         killer = `${killer[0].toUpperCase()}${killer.substring(1).replace(/_/g, ' ').replace(/(?<= )./g, (char) => `${char.toUpperCase()}`)}`
         switch (arg.damageSource.cause) {
             case 'entityExplosion':
-                embed['embeds']['description'] = `${arg.deadEntity.name} ${deathMessages[arg.damageSource.cause]} ${killer.replace(/§./g, '').replace(/(\n).+/, '')}.`
+                embed[0]['description'] = `${arg.deadEntity.name} ${deathMessages[arg.damageSource.cause]} ${killer.replace(/§./g, '').replace(/(\n).+/, '')}.`
                 break;
             case 'entityAttack':
-                embed['embeds']['description'] = `${arg.deadEntity.name} ${deathMessages[arg.damageSource.cause]} ${killer.replace(/§./g, '').replace(/(\n).+/, '')}.`
+                embed[0]['description'] = `${arg.deadEntity.name} ${deathMessages[arg.damageSource.cause]} ${killer.replace(/§./g, '').replace(/(\n).+/, '')}.`
                 break;
             case 'magic':
-                embed['embeds']['description'] = `${arg.deadEntity.name} was slain by ${killer.replace(/§./g, '').replace(/(\n).+/, '')} using magic.`
+                embed[0]['description'] = `${arg.deadEntity.name} was slain by ${killer.replace(/§./g, '').replace(/(\n).+/, '')} using magic.`
                 break;
             case 'projectile':
-                embed['embeds']['description'] = `${arg.deadEntity.name} was shot by ${killer.replace(/§./g, '').replace(/(\n).+/, '')}.`
+                embed[0]['description'] = `${arg.deadEntity.name} was shot by ${killer.replace(/§./g, '').replace(/(\n).+/, '')}.`
                 break;
             case 'contact':
-                embed['embeds']['description'] = `${arg.deadEntity.name} was probably trying to fuck with a cactus.`
+                embed[0]['description'] = `${arg.deadEntity.name} was probably trying to fuck with a cactus.`
                 break;
             default:
                 break;
         }
+        world.sendMessage(embed[0]['description'])
     } else {
-        embed['embeds']['description'] = `${arg.deadEntity.name} ${deathMessages[arg.damageSource.cause]}.`
+        embed[0]['description'] = `${arg.deadEntity.name} ${deathMessages[arg.damageSource.cause]}.`
     };
-    const request = new HttpRequest(webHookURL);
-    request.setBody(
-        JSON.stringify(
-            embed
-        )
-    );
-    request.setMethod(HttpRequestMethod.POST);
-    request.setHeaders([new HttpHeader("Content-Type", "application/json")]);
-    http.request(request);
+    weebhookMessage(undefined, embed);
 });
